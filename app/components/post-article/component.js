d51de34291408be0a86a83b19c47e5e8d030072c
Jack Flannery <jmflannery81@gmail.com>
2016-10-29 22:42:59 -0400

    Add highlighjs bower component

--- app/components/post-article/component.js
+++ app/components/post-article/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'article',
+
+  didRender: function() {
+    this.$('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
+  }
 });

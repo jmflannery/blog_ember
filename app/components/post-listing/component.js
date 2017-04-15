be15e917910181998b048bb1440eed7c785dd60d
Jack Flannery <jmflannery81@gmail.com>
2017-04-15 17:02:36 -0400

    Publish post functionality

--- app/components/post-listing/component.js
+++ app/components/post-listing/component.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    publishPost() {
+      let post = this.get('post')
+      post.set('published_at', new Date());
+      post.save();
+    },
+
     deletePost() {
       this.get('post').destroyRecord();
     }
--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   content: DS.attr(),
-  created_at: DS.attr()
+  created_at: DS.attr(),
+  updated_at: DS.attr(),
+  published_at: DS.attr()
 });

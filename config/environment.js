7a3f846b5161920fd1c016acdc28aa952750279b
Jack Flannery <jmflannery81@gmail.com>
2017-11-12 11:19:23 -0500

    Update prod api url

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.apiUrl = 'http://jackflannery.blog';
+    ENV.apiUrl = 'http://jackflannery.blog/api';
   }
 
   return ENV;
